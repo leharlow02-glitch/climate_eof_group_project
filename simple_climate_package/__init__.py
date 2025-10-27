57df45ae2481c737ea670f40766a168c07d0d100
Hannah-Jane Wood <shil6825@ox.ac.uk>
2025-10-27 12:50:12 +0000

    create a folder for our package

---
+++ simple_climate_package/__init__.py
@@ -0,0 +1,9 @@
+"""simple_climate_package is a Climate analysis library.
+
+It contains functionality for downloading, analysing, and plotting
+temperature data over Europe
+"""
+# Import version info
+from .version_info import VERSION_INT, VERSION  # noqa
+
+# Import main classes
