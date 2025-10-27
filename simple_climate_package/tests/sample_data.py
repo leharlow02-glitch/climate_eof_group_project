51130a8f06921a8d780469813d4cea8cd6c75b18
Hannah-Jane Wood <shil6825@ox.ac.uk>
2025-10-27 17:07:58 +0000

    switch lons and lats in sample_data.py

--- simple_climate_package/tests/sample_data.py
+++ simple_climate_package/tests/sample_data.py
@@ -9,17 +9,17 @@ def make_sample_era5_t2m():
     # times gives daily timestamps
     times = pd.date_range("2020-01-01", "2021-12-31", freq="D")
     # lats gives 3 grid points between -10 and 10
-    lats = np.linspace(-10, 10, 3)
-    # lons gives 3 grid points between 100 and 110
     lons = np.linspace(100, 110, 3)
+    # lons gives 3 grid points between 100 and 110
+    lats = np.linspace(-10, 10, 3)
     # data generates synthetic temperature data for every timestamp, latitude, and longitude
     # multiplying by 10 and then adding 273.15 gives a range of 0-10 degrees celcius
-    data = 273.15 + 10*np.random.rand(len(times), len(lats), len(lons))
+    data = 273.15 + 10*np.random.rand(len(times), len(lons), len(lats))
 
     # this wraps the created data into an xarray dataset
     ds = xr.Dataset(
-        {"t2m": (("time", "latitude", "longitude"), data)},
-        coords={"time": times, "latitude": lats, "longitude": lons},
+        {"t2m": (("time", "longitude", "latitude"), data)},
+        coords={"time": times, "longitude": lons, "latitude": lats},
         attrs={"description": "Synthetic ERA5-like temperature dataset (for tests)"}
     )
     return ds
