4f1daf4403fa2a7c74f64095bb2180271d40ab39
Hannah-Jane Wood <shil6825@ox.ac.uk>
2025-10-27 15:23:47 +0000

    Create a sample dataset and test python file

---
+++ simple_climate_package/tests/sample_data.py
@@ -0,0 +1,25 @@
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+def make_sample_era5_t2m():
+    # make a sample ERA5 t2m dataset
+    # our code expects the same coordinate structure of time, latitude, longitude, t2m
+    # this dataset keeps to the expected coordinate structure:
+    # times gives daily timestamps
+    times = pd.date_range("2020-01-01", "2021-12-31", freq="D")
+    # lats gives 3 grid points between -10 and 10
+    lats = np.linspace(-10, 10, 3)
+    # lons gives 3 grid points between 100 and 110
+    lons = np.linspace(100, 110, 3)
+    # data generates synthetic temperature data for every timestamp, latitude, and longitude
+    # multiplying by 10 and then adding 273.15 gives a range of 0-10 degrees celcius
+    data = 273.15 + 10*np.random.rand(len(times), len(lats), len(lons))
+
+    # this wraps the created data into an xarray dataset
+    ds = xr.Dataset(
+        {"t2m": (("time", "latitude", "longitude"), data)},
+        coords={"time": times, "latitude": lats, "longitude": lons},
+        attrs={"description": "Synthetic ERA5-like temperature dataset (for tests)"}
+    )
+    return ds
