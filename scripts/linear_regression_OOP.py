1327d3ec3f9e08271986d793a3df0f64f650d38a
Lucy Harlow <leharlow02@gmail.com>
2025-10-28 14:25:36 +0000

    Created a file to translate linear regression functions from linear_regression.py into object oriented style

---
+++ scripts/linear_regression_OOP.py
@@ -0,0 +1,11 @@
+from scipy import stats
+import numpy as np
+from scipy.stats import t
+import matplotlib.pyplot as plt
+import xarray as xr
+import matplotlib.colors as mcolors
+
+###Writing linear regression calculation and plotting class###
+
+
+
