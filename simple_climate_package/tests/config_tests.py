a45c144a8eab90d92e48a3ca3b5c1b5d1b4ed760
Hannah-Jane Wood <shil6825@ox.ac.uk>
2025-10-28 16:17:52 +0000

    delete uneeded test files

--- simple_climate_package/tests/config_tests.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from tests.sample_data import make_sample_era5_t2m
-
-
-@pytest.fixture
-def sample_t2m_dataset():
-    # this fixture creates a small synthetic ERA5 dataset in memory
-    return make_sample_era5_t2m()
